f7fcf4a6142f7b45b7543908424cdec7de22b535
solomon4000 <slam60@charlottesvilleschools.org>
2023-08-29 14:42:24 -0400

    i made changes

---
+++ code.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials Servo standard servo example"""
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+from digitalio import DigitalInOut, Direction, Pull
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+switch = DigitalInOut(board.D7)
+switch2 = DigitalInOut(board.D8)
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+
+while True:
+    while switch:
+        angle--;
+        my_servo.angle = angle
+        time.sleep(0.05)
