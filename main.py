ddfdaedce86d3adf662df5c45dd0fde0bce06e61
solomon4000 <slam60@charlottesvilleschools.org>
2023-09-05 15:08:31 -0400

    Changed it

--- main.py
+++ main.py
@@ -3,20 +3,31 @@
 # SPDX-License-Identifier: MIT
 
 """CircuitPython Essentials Servo standard servo example"""
+import touchio
 import time
 import board
 import pwmio
 from adafruit_motor import servo
 from digitalio import DigitalInOut, Direction, Pull
-
+angle=0
 # create a PWMOut object on Pin A2.
 pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
-switch = DigitalInOut(board.D7)
-switch2 = DigitalInOut(board.D8)
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
+touch_pad = board.A0  # Will not work for Circuit Playground Express!
+touch_pad2=board.A1
+touch = touchio.TouchIn(touch_pad)
+touch2 = touchio.TouchIn(touch_pad2)
 
-while switch:
-    angle--
-    my_servo.angle = angle
+while True:
+    if touch.value:
+        if angle>=180:
+            angle=0
+        angle=angle-1
+        my_servo.angle = angle
+    if touch2.value:
+        if angle<=0:
+            angle=179
+        angle=angle+1
+        my_servo.angle = angle
     time.sleep(0.05)
