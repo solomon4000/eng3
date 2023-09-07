d9b38307f9b46573ce99611791a080c8694ebbfd
solomon4000 <slam60@charlottesvilleschools.org>
2023-09-07 15:23:21 -0400

    Ultrasonic sensor

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
-#towihriuweqhruiywqegr8owrge tesyt tests tetstets tst estsetsetset tsstese test
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-"""CircuitPython Essentials Servo standard servo example"""
 import time
 import board
-import pwmio
-from adafruit_motor import servo
-from digitalio import DigitalInOut, Direction, Pull
+import adafruit_hcsr04
 
-# create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
-switch = DigitalInOut(board.D7)
-switch2 = DigitalInOut(board.D8)
-# Create a servo object, my_servo.
-my_servo = servo.Servo(pwm)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+
+while True:
+    try:
+        print((sonar.distance,))
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.1)
