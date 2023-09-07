8e170b0bbffa4c79ee62a33a4ea79a19b3f5768a
solomon4000 <slam60@charlottesvilleschools.org>
2023-09-07 15:43:26 -0400

    Made changes

--- distance.py
+++ distance.py
@@ -4,12 +4,17 @@
 import time
 import board
 import adafruit_hcsr04
-
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+import neopixel
 
+led = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+led.brightness = 1.0
 while True:
     try:
         print((sonar.distance,))
     except RuntimeError:
         print("Retrying!")
     time.sleep(0.1)
+    led=(0, sonar.distance, 255-sonar.distance) 
+    
