842586eca71c4434885534d7477f535cc41d7e1b
solomon4000 <slam60@charlottesvilleschools.org>
2023-09-25 15:12:24 -0400

    This might or might not work 💀💀💀

--- led_seizure_machine.py
+++ led_seizure_machine.py
@@ -1,21 +1,21 @@
 # SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
 # SPDX-License-Identifier: MIT
+# Build this circuit with two 100W COB leds being driven with an irf520 MOSFET
+# Trust me don't do it 💀💀💀
 """CircuitPython Blink Example - the CircuitPython 'Hello, World!'"""
 import time
 import board
-import random
 import digitalio
 
-led = digitalio.DigitalInOut(board.D4)
+led = digitalio.DigitalInOut(board.LED)
+led2=digitalio.DigitalInOut(board.D4)
+led2.direction=digitalio.Direction.OUTPUT
 led.direction = digitalio.Direction.OUTPUT
-led2 = digitalio.DigitalInOut(board.D3)
-led2.direction = digitalio.Direction.OUTPUT
 
 while True:
-    ledDelay=0.05
     led.value = True
-    led2.value=False
-    time.sleep(ledDelay)
+    led.value=False
+    time.sleep(0.1)
     led.value = False
-    led2.value=True
-    time.sleep(ledDelay)
+    led.value=True
+    time.sleep(0.1)
