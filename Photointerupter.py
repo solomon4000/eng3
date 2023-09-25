712f43fe6b1d075241f9e184e1f48b606b4254f4
solomon4000 <slam60@charlottesvilleschools.org>
2023-09-25 15:14:50 -0400

    This might or might not work 💀💀💀

---
+++ Photointerupter.py
@@ -0,0 +1,23 @@
+#this shouldnt be so difficult
+#save me and all of addies mosfets
+#Why won't it work?
+import time
+import digitalio
+import board
+from digitalio import DigitalInOut, Direction, DriveMode, Pull
+print("srdg")
+photointerupter=digitalio.DigitalInOut(board.D7)
+photointerupter.direction.INPUT
+photointerupter.pull = Pull.UP
+now = time.monotonic()  # Time in seconds since power on
+count=0
+thing=False
+while True:
+    if photointerupter.value:
+         count=count+1
+         print("test")
+    if (now + 4) < time.monotonic():  # If 3 milliseconds elapses
+        print(count)
+        count=0
+        now = time.monotonic()
+    time.sleep(0.05)
